185317a999008596f518b1381b32e1d79c50c569
unknown <lwrow@rambler.ru>
2025-03-04 00:50:54 +0300

    modified Main, Util, User, UserServiceImpl, UserDaoHibernateImpl

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -1,10 +1,19 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
 
 import java.util.List;
+import java.util.logging.Logger;
 
 public class UserDaoHibernateImpl implements UserDao {
+    private static final Logger logger = Logger.getLogger(UserDaoHibernateImpl.class.getName());
+    private static final String CREATE_TABLE = "CREATE TABLE IF NOT EXISTS User" +
+            "(id BIGINT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), lastName VARCHAR(255), age INT)";
+    private static final String DROP_TABLE = "DROP TABLE IF EXISTS User";
+
     public UserDaoHibernateImpl() {
 
     }
@@ -12,31 +21,125 @@ public class UserDaoHibernateImpl implements UserDao {
 
     @Override
     public void createUsersTable() {
-
+        Transaction transaction = null;
+        try (Session session = Util.getSessionFactory().openSession()) {
+            transaction = session.beginTransaction();
+        session.createSQLQuery(CREATE_TABLE).executeUpdate();
+        transaction.commit();
+        session.close();
+            logger.info("User table was created.");
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            logger.severe("Creating User table failed with message: "
+                    + e.getMessage());
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public void dropUsersTable() {
-
+        Transaction transaction = null;
+        try (Session session = Util.getSessionFactory().openSession()) {
+            transaction = session.beginTransaction();
+        session.createSQLQuery(DROP_TABLE).executeUpdate();
+        transaction.commit();
+        session.close();
+            logger.info("User table was dropped.");
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            logger.severe("Dropping User table failed with message: "
+                    + e.getMessage());
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
-
+        Transaction transaction = null;
+        try (Session session = Util.getSessionFactory().openSession()) {
+            transaction = session.beginTransaction();
+        session.save(new User(name, lastName, age));
+        transaction.commit();
+        session.close();
+            logger.info("User was saved.");
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            logger.severe("Saving user with name - "
+                    + name
+                    + "/n last name - "
+                    + lastName
+                    + "/n age "
+                    + "/n failed with message: "
+                    + e.getMessage());
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public void removeUserById(long id) {
-
+        Transaction transaction = null;
+        try (Session session = Util.getSessionFactory().openSession()) {
+            transaction = session.beginTransaction();
+        User user = (User) session.get(User.class, id);
+        session.delete(user);
+        transaction.commit();
+        session.close();
+            logger.info("User with id " + id + " was removed from DB.");
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            logger.severe("Removing user with id "
+                    + id
+                    + " failed with message: "
+                    + e.getMessage());
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public List<User> getAllUsers() {
-        return null;
+        Transaction transaction = null;
+        List<User> users = null;
+        try (Session session = Util.getSessionFactory().openSession()) {
+            transaction = session.beginTransaction();
+            users = session.createQuery("from User").list();
+            transaction.commit();
+            session.close();
+            logger.info("UserList was got.");
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            logger.severe("Getting userList failed with message: "
+                    + e.getMessage());
+            throw new RuntimeException(e);
+        }
+        return users;
     }
 
     @Override
     public void cleanUsersTable() {
-
+        Transaction transaction = null;
+        try (Session session = Util.getSessionFactory().openSession()) {
+            transaction = session.beginTransaction();
+            session.createQuery("delete from User").executeUpdate();
+            transaction.commit();
+            session.close();
+            logger.info("User table was cleaned.");
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            logger.severe("Cleaning up the User table failed with message: "
+                    + e.getMessage());
+            throw new RuntimeException(e);
+        }
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,46 @@
 package jm.task.core.jdbc.util;
 
+import jm.task.core.jdbc.model.User;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+
+import java.util.logging.Logger;
+
 public class Util {
     // реализуйте настройку соеденения с БД
+    private static final String DIALECT = "org.hibernate.dialect.MySQLDialect";
+    private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
+    private static final String USER_NAME = "root";
+    private static final String PASSWORD = "qwerty12";
+    private static final String URL = "jdbc:mysql://localhost:3306/pp_1_1_3";
+
+    private static Logger log = Logger.getLogger(Util.class.getName());
+
+    private static SessionFactory sessionFactory;
+
+    private Util() {}
+
+    public static SessionFactory getSessionFactory() {
+        if (sessionFactory == null) {
+            try {
+                Configuration configuration = new Configuration()
+                .setProperty("hibernate.dialect", DIALECT)
+                .setProperty("hibernate.connection.driver_class", DRIVER)
+                .setProperty("hibernate.connection.url", URL)
+                .setProperty("hibernate.connection.username", USER_NAME)
+                .setProperty("hibernate.connection.password", PASSWORD)
+                .setProperty("hibernate.hbm2ddl.auto", "update")
+                .addAnnotatedClass(User.class);
+                StandardServiceRegistryBuilder builder = new StandardServiceRegistryBuilder()
+                        .applySettings(configuration.getProperties());
+                sessionFactory = configuration.buildSessionFactory(builder.build());
+                log.info("SessionFactory created");
+            } catch (Exception e) {
+                log.severe("SessionFactory creation failed with message: " + e.getMessage());
+            }
+        }
+        return sessionFactory;
+    }
+
 }
